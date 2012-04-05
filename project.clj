b71483df1a92fb4d204dce76d9629491d3f1efd9
Daniel Westheide <daniel@doo.net>
2012-04-05 09:52:48 +0200

    Switch to Clojure 1.2.1 in the project dependencies.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject avout "0.5.3"
+(defproject avout "0.5.4-SNAPSHOT"
   :description "Avout: Distributed State in Clojure"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [zookeeper-clj "0.9.1"]]
   :dev-dependencies [[lein-clojars "0.7.0"]])
